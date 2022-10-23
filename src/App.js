e5281d1eed693bc5e619bb2f7f0f027813057892
Atul Kumar Gupta <54747380+AtulKumarIndian@users.noreply.github.com>
2022-10-23 17:49:26 +0530

    working demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import './App.css';
 
+import React, {useState} from 'react';
+
 function App() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+  function nextPage(){
+    
+    setPageNumber(pageNumber+1);
+  }
+  function prevPage(){
+    if(pageNumber > 1)
+    setPageNumber(pageNumber-1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <center>
+      <Document file="reactjs.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <p>
+        Page {pageNumber} of {numPages}
+      </p>
+      <button onClick={prevPage}>prev</button>
+      <button onClick={nextPage}>next</button>
+     
+      </center>
+      
     </div>
   );
 }
